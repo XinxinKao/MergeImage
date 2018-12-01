4c733a90f980cdb510ea69961f6eea5072f394a5
xinyi <xinyi.kao@titansoft.com.sg>
2018-12-01 23:29:24 +0800

    Add Image file and ImageHelper class

---
+++ MergeImage/MergeImage/ImageHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+
+namespace MergeImage
+{
+	public class ImageHelper
+	{
+		public void MergeImage()
+		{
+			var bgImg = Image.FromFile("../../img/background.png");
+			var logo = Image.FromFile("../../img/Usagi.png");
+
+			var graphics = Graphics.FromImage(bgImg);
+
+			using (var target = Graphics.FromImage(logo))
+			{
+				target.DrawImage(logo, new Rectangle(0, 0, logo.Width, logo.Height), 0, 0, logo.Width, logo.Height, GraphicsUnit.Pixel);
+				target.Dispose();
+			}
+
+			var logoX = bgImg.Width - logo.Width + 50;
+			var logoY = 5;
+			graphics.DrawImage(logo, logoX, logoY);
+			graphics.Dispose();
+			var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+			var filePath = $"{path}/../../ResultImg/Result{DateTime.Now:MMddhhmmss}.jpg";
+			bgImg.Save(filePath, ImageFormat.Jpeg);
+		}
+	}
+}
